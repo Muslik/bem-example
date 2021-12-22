375b43a87360aaffaaf5f953bf950cd3c2f82dd8
Dzhabrail <dzhabulya@gmail.com>
2021-12-22 22:36:31 +0300

    Added files

---
+++ main.js
@@ -0,0 +1,13 @@
+const elements = document.getElementsByClassName("accordion");
+
+for (let i = 0; i < elements.length; i++) {
+  elements[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
